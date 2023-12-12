d6104bb0b342065f765fe40a22aea9ad51c50b69
Zarathouscode <137414336+Zarathouscode@users.noreply.github.com>
2023-12-12 10:32:03 +0100

    Update utils.py

--- discord/utils.py
+++ discord/utils.py
@@ -1471,20 +1471,31 @@ async def _get_info(session: ClientSession) -> Tuple[Dict[str, Any], str]:
     return properties, b64encode(_to_json(properties).encode()).decode('utf-8')
 
 
-async def _get_build_number(session: ClientSession) -> int:  # Thank you Discord-S.C.U.M
+async def _get_build_number(session: ClientSession) -> int:
     """Fetches client build number"""
-    default_build_number = 9999
     try:
         login_page_request = await session.get('https://discord.com/login', timeout=7)
         login_page = await login_page_request.text()
-        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)\.js').findall(login_page)[-2] + '.js'
-        build_request = await session.get(build_url, timeout=7)
-        build_file = await build_request.text()
-        build_find = re.findall(r'Build Number:\D+"(\d+)"', build_file)
-        return int(build_find[0]) if build_find else default_build_number
-    except asyncio.TimeoutError:
+        # Find all potential JavaScript files that could contain the build number
+        js_files = re.findall(r'assets/([a-z0-9]+)\.js', login_page)
+        # Check each JavaScript file for the build number
+        for js_file in js_files:
+            build_url = f'https://discord.com/assets/{js_file}.js'
+            build_request = await session.get(build_url, timeout=7)
+            build_file = await build_request.text()
+            # Look for 'buildNumber' in the JS file
+            build_index = build_file.find('buildNumber') + len('buildNumber') + 2  # Adjusting the offset to after "buildNumber": 
+            # Try to extract a six-digit number, assuming that the build number is 6 digits
+            possible_build_number = build_file[build_index: build_index + 6]
+            # If we find a number, return it
+            if possible_build_number.isdigit():
+                return int(possible_build_number)
+        # If no build number is found in any file, fall back to the hardcoded value
         _log.critical('Could not fetch client build number. Falling back to hardcoded value...')
-        return default_build_number
+        return 244594
+    except asyncio.TimeoutError as e:
+        _log.critical(f'Could not fetch client build number due to timeout: {e}')
+        return 244594
 
 
 async def _get_user_agent(session: ClientSession) -> str:
